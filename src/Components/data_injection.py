842e3e09b91682101cc927a68f9ae1c16679cb86
BabaKaElijah <tsebeelias@gmail.com>
2025-11-19 12:26:51 +0200

    Model_Trainer Added

--- src/Components/data_injection.py
+++ src/Components/data_injection.py
@@ -1,5 +1,6 @@
-import os
 import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from src.exception import CustomException
 from src.logger import logging 
 import pandas as pd 
@@ -9,6 +10,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,5 +55,8 @@ if __name__=="__main__":
   train_data,test_data=obj.initiate_data_injection()    
 
   data_transformation=DataTransformation()
-  data_transformation.initiate_data_transformation(train_data,test_data)
+  train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+  modeltrainer=ModelTrainer()
+  print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
     
